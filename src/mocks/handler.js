21ec0b8a12d76d4e96b9c6ef8be9ec8ab149495e
jooam <lord0325@naver.com>
2022-04-26 20:55:18 +0900

    [fix] MSW favicon 경고 처리

--- src/mocks/handler.js
+++ src/mocks/handler.js
@@ -6,9 +6,9 @@ const mockPosts = (function makePostData() {
   for (let i = 0; i < 100; i++) {
     result.push({
       id: i,
-      title: `title${i}`,
+      title: `title${i} Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem libero tempore excepturi. Inventore, dolor aut modi, velit repellat quos ad minima similique, voluptatum deleniti ipsam. Blanditiis est eum similique deserunt? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem libero tempore excepturi. Inventore, dolor aut modi`,
       writer: `writer${i}`,
-      content: `${i} Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem libero tempore excepturi. Inventore, dolor aut modi, velit repellat quos ad minima similique, voluptatum deleniti ipsam. Blanditiis est eum similique deserunt?`,
+      content: `${i} Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem libero tempore excepturi. Inventore, dolor aut modi, velit repellat quos ad minima similique, voluptatum deleniti ipsam. Blanditiis est eum similique deserunt? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem libero tempore excepturi. Inventore, dolor aut modi, velit repellat quos ad minima similique, voluptatum deleniti ipsam. Blanditiis est eum similique deserunt?`,
       sympathies: parseInt(Math.random() * (i + 5)),
       comments: parseInt(Math.random() * (i + 1)),
     });
@@ -26,5 +26,8 @@ export const handler = [
   }),
   rest.post("/register", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(req.body));
+  }),
+  rest.get("/favicon.ico", (req, res, ctx) => {
+    return res(ctx.status(200));
   }),
 ];
